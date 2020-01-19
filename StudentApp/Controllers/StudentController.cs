cbb9ef9b8a27b288f612b4e3b925ae4e12f35f16
VeraGritcu <vera.gritcu@gmail.com>
2020-01-19 18:51:33 +0200

    cvbn

---
+++ StudentApp/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using StudentApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> l = GetAllStudents();
+
+            return View(l);
+        }
+
+        private static List<Student> GetAllStudents()
+        {
+            List<Student> l = new List<Student>();
+            l.Add(new Student { Age = 22, StudentId = 1, StudentName = "Mark Twain", Specialization = "Mathematics", University = "Some University1"});
+
+            l.Add(new Student { Age = 20, StudentId = 2, StudentName = "Victoria Brown", Specialization = "Geography", University = "Some University2" });
+
+            l.Add(new Student { Age = 21, StudentId = 3, StudentName = "Vera Lee", Specialization = "History", University = "Some University2" });
+
+            l.Add(new Student { Age = 22, StudentId = 4, StudentName = "Ashton Twin", Specialization = "Diplomacy", University = "Some University2" });
+
+            l.Add(new Student { Age = 22, StudentId = 1, StudentName = "Mark Twain", Specialization = "Sience", University = "Some University1" });
+            return l;
+        }
+
+        private static Student GetStudentById(int id)
+        {
+            List<Student> l = GetAllStudents();
+
+            var s = l.FirstOrDefault(p => p.StudentId == id);
+            return s;
+        }
+
+        [ActionName ("Details")]
+        public ActionResult GetById (int id)
+        {
+
+            Student s = GetStudentById(id);
+
+            return View(s);
+        }
+    }
+}
---
+++ StudentApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+        public string Specialization { get; set; }
+        public string University { get; set; }
+
+    }
+}
